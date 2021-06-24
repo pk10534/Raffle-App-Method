5828f0669c1318a87fbe4b8ee3d8e8ccc6d412ee
Patrick K <pk10534@hotmail.com>
2021-06-24 10:03:54 -0400

    Added in comments - final version

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleUI
         private static Random rdm = new Random();
 
         
-        //Method to receive user input w/o doing console.write/readline
+        //Method to receive user input w/o doing console.write/readline. This takes their input, stores it in a string, and returns that string
         private static string GetUserInput(string message)
         {
             Console.WriteLine(message);
@@ -27,15 +27,15 @@ namespace ConsoleUI
             return response;
         }
         //Method to generate a random number for the raffle
-        private static int GenerateRandomNumber(int min, int max)
+        private static int GenerateRandomNumber(int min, int max) //here establish two integers will be put in place for the method to utilize
         {
-            int newNum = rdm.Next(min, max);
-            return newNum;
+            int newNum = rdm.Next(min, max); //this tells it to use those variables (min & max) and generate a random number from them
+            return newNum; //return the newly generated random number
         }
         //Method to add in guest name + their raffle number into our guests dictionary
-        private static void AddGuestsInRaffle(int raffleNumber, string guest)
+        private static void AddGuestsInRaffle(int raffleNumber, string guest) //this is set up to mirror our "guest" dict: Dict<int, string>
         {
-            guests.Add(raffleNumber, guest);
+            guests.Add(raffleNumber, guest); 
         }
 
 
@@ -50,42 +50,42 @@ namespace ConsoleUI
             do //do the following: 
             {
                 otherGuest = GetUserInput("Please enter a name: ").ToLower(); //print out quoted statement, then return that in a string called otherGuest
-                randomRaffleNumber = GenerateRandomNumber(min, max);
-                AddGuestsInRaffle(randomRaffleNumber, otherGuest);
+                randomRaffleNumber = GenerateRandomNumber(min, max); //generate random number between 1000 and 9999
+                AddGuestsInRaffle(randomRaffleNumber, otherGuest); //add the inputed name and generated raffle number to dictioanry "guests'
                 Console.WriteLine($"The name '{otherGuest}' has been succesfully recorded and their raffle number is {randomRaffleNumber}."); 
                 Console.WriteLine("Would you like to enter another name? Enter 'yes' or 'no': "); 
-                response1 = Console.ReadLine();
+                response1 = Console.ReadLine(); //stores user response so that "while" loop may make decisions on whether to repeat loop or not
             } while (response1 == "yes" || response1 == ""); //continue looping "otherGuest" etc. while the response to "would you like to enter another name" is equal to "yes" or empty
         }
 
         private static void PrintGuestsName()
         {
-            foreach (var personName in guests)
+            foreach (var personName in guests) //for each instance in our dict guests, run through and list them
             {
-                Console.WriteLine(personName);
+                Console.WriteLine(personName); 
             }
         }
 
         private static int GetRaffleNumber(Dictionary<int, string> guests)
         {
-            int index;
-            int winner;
-            KeyValuePair<int, string> pair;
-            for (int i = 0; i < 10; i++) 
+            int index; //arbitrary name, important for our "for" loop below
+            int winner; 
+            KeyValuePair<int, string> pair; //"pair" is going to hold both the key and value. it is a placeholder name for our dictionary
+            for (int i = 0; i < 10; i++) //when i is less than 10, going up from zero, keep looping through and what comes next...
             {
-                index = rdm.Next(guests.Count);
-                pair = guests.ElementAt(index);
-                winner = pair.Key;
-                return winner;
+                index = rdm.Next(guests.Count); //make the index int a random number equal to however many key/value pairs are in guests
+                pair = guests.ElementAt(index); //our pair identifier is equal to whichever index rdm.Next finds
+                winner = pair.Key; //we only want the key right now
+                return winner; //so return that key
             }
             return 1;
         }
 
         private static void PrintWinner()
         {
-            int winnerNumber = GetRaffleNumber(guests); 
-            string winnerName = guests[winnerNumber];
-            Console.WriteLine($"The Winner is: {winnerName} with the raffle #{winnerNumber}");
+            int winnerNumber = GetRaffleNumber(guests); //call the above method for dictionary "guests". store that into our new integer
+            string winnerName = guests[winnerNumber]; //we can use that integer (as a key) to access the value of that indice as well
+            Console.WriteLine($"The Winner is: {winnerName} with the raffle #{winnerNumber}"); //finalize writing it all out and we're done!
         }
 
 
@@ -98,7 +98,7 @@ namespace ConsoleUI
             GetUserInfo();
             PrintGuestsName();
             PrintWinner();
-            Console.ReadLine();
+            Console.ReadLine(); //added this in because my console pop-up window kept closing 
         }
 
         //Start writing your code here
